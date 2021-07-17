9a8efd8c7e9f0616f2ea8f6451788690918f4ace
oralsepa <orlandosedano@yahoo.es>
2021-07-17 14:25:16 -0500

    ActualizaNombreRepositorio

--- src/EjerciciosOASP.java
+++ src/EjerciciosOASP.java
@@ -12,10 +12,10 @@ git o github
     git add README.md
     git commit -m "first commit"
     git branch -M main
-    git remote add origin https://github.com/oralsepa/ciclo2.git
+    git remote add origin https://github.com/oralsepa/ciclo2mintic.git
     git push -u origin main
 …or push an existing repository from the command line
-    git remote add origin https://github.com/oralsepa/ciclo2.git
+    git remote add origin https://github.com/oralsepa/ciclo2mintic.git
     git branch -M main
     git push -u origin main
 
@@ -75,6 +75,26 @@ public class EjerciciosOASP {
         for(ini;condicion;incremento){
 
         }*/
+
+        String s1 = "Orlando ";
+        String s2 = s1 + "Alberto";
+        String s3 = s1 + s2 + " Sedano";
+
+        s3.length(); //longitud de la cadena
+        s3.indexOf('s'); //posición de la primera aparición, -1 si no existe
+        s3.lastIndexOf('b'); //posición de la última aparición, -1 si no existe
+        s3.chartAt(11); //carácter en la posición n
+        s3.substring(5,12); //subcadena de la posición n1 a n2-1
+        s3.toUpperCase(); //mayúsculas
+        s3.toLowerCase(); //minúsculas
+        s3.equals(s1); //colmpara las cadenas y devuelve "true" si son iguales
+        s3.equalsIgnoreCase(s1); //similar al anterior sin consuderar mayúsculas o minúsculas
+        valueOf(2344); //convierte el dato a string
+        s3.trim(); //elimina los espacios en blanco de los extremos
+
+        
+
+
         
     }
 }
